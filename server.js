c8a4c02c7e0d347bb98155205fac7b8c521df8f1
amritsingh99 <amrit1438@gmail.com>
2023-01-26 20:05:55 +0530

    cosmetic changes
    renamed notes to orders

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 import express from 'express'
-
+import OrderStore from './orders-mongodb.js';
 const app = express()
 
 app.listen(5001, () => {
     console.log('API running on port 5001')
 })
 
-app.get('/', (req, res) => {
-    res.json('Data API running')
+app.get('/findAll', async (req, res) => {
+    try {
+        const orders = new OrderStore();
+        const data = await orders.readAll();
+        res.json(data)
+    } catch (err) {
+        console.error(err)
+    }
 })
